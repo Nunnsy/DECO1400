4f7f9884cbac14b493b1cd7cae9ec1240f23ca44
Nunnsy <danielbnunns@gmail.com>
2016-05-15 12:49:23 +1000

    Changed on-click reset method to a jQuery listener.

--- js/reset.js
+++ js/reset.js
@@ -1,6 +1,12 @@
 // Define a boolean variable that is accessible in multiple functions.
 var confirm_shown = false;
 
+$(document).ready(function() {
+  $(".reset-button").click(function() {
+    resetGame();
+  });
+});
+
 // The function called when the reset button is pressed.
 function resetGame() {
     // Check if the confirmation has already been shown.
@@ -9,7 +15,7 @@ function resetGame() {
         confirmReset();
     } else {
         // Change the reset button text to a confirmation queue for the user.
-        $('.reset-button').text('Are you sure?');
+        $(".reset-button").text("Are you sure?");
         confirm_shown = true;
     }
 }
@@ -17,21 +23,21 @@ function resetGame() {
 // The function which resets the game.
 function confirmReset() {
     // Remove the story path data.
-    localStorage.removeItem('storyPath');
+    localStorage.removeItem("storyPath");
 
     // Check if a current story lockout exists.
-    if (localStorage.getItem('storyUnlock') != null) {
+    if (localStorage.getItem("storyUnlock") != null) {
         // If it does exist, remove it from the local storage.
-        localStorage.removeItem('storyUnlock');
+        localStorage.removeItem("storyUnlock");
     }
 
     // Gracefully remove the reset button from view.
-    $('.reset-button').addClass('reset-button-hidden');
-    $('.reset-container').addClass('animated fadeOut');
+    $(".reset-button").addClass("reset-button-hidden");
+    $(".reset-container").addClass("animated fadeOut");
 }
 
 // Function is run on page load to check if game data exists.
-if (localStorage.getItem('storyPath') != null) {
+if (localStorage.getItem("storyPath") != null) {
     // Show the reset button if game data exists.
-    $('.reset-container').removeClass('hide');
+    $(".reset-container").removeClass("hide");
 }
