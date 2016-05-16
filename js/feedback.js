2d2adf9c7b28483028efdfc59112e557b786bf52
Nunnsy <danielbnunns@gmail.com>
2016-05-16 16:39:59 +1000

    Added more animations & polished variable accessors.

--- js/feedback.js
+++ js/feedback.js
@@ -1,11 +1,20 @@
 // When the document is ready, execute the following code.
 $(document).ready(function() {
-  // Find the form on the page and add a 'Submit' listener.
+
+    // Declare the form objects for use throughout code.
+    var feedbackNameObject = $("#feedbackName");
+    var feedbackMessageObject = $("#feedbackMessage");
+
+    // Find the form on the page and add a 'Submit' listener.
     $("#newFeedbackForm").submit(function(event) {
         var complete = false;
 
+        // Get the ossociated values within the inputs.
+        var name = feedbackNameObject.val();
+        var message = feedbackMessageObject.val();
+
         // If all inputs are filled, the form is complete.
-        if ($("#feedbackName").val() != "" && $("#feedbackMessage").val() != "") {
+        if (name != "" && message != "") {
             complete = true;
         }
 
@@ -13,42 +22,46 @@ $(document).ready(function() {
             // Grabs a feedback article and clones it from the HTML so we don't create one from scratch.
             var newFeedback = $("article:last-of-type").clone();
 
-            // Gets the values from the form.
-            var name = $("#feedbackName").val();
-            var feedback = $("#feedbackMessage").val();
-
             // Replace the cloned text with the form specific values.
             newFeedback.find(".user-name").text(name);
             newFeedback.find(".user-date").text(getTodayAsString());
-            newFeedback.find(".user-feedback").text(feedback);
+            newFeedback.find(".user-feedback").text(message);
 
             // Add the new feedback to the page. (reverse chronological order)
             $("form").parent().after(newFeedback);
 
             // Animate the form's exit.
-            $("form").parent().slideUp("slow");
+            $("form").parent().slideUp(1000);
+
+            // Animate the feedback entrance.
+            // Display none must be added to start the element in the 'slid up' position.
+            newFeedback.css("display", "none");
+            newFeedback.slideDown(1000);
         } else {
-            // Check to see which inputs are filled and highlight the label if not filled.
-            // We also check if they've corrected an input but not another and remove the error highlighting.
-            if ($("#feedbackName").val() == "") {
-                $("#feedbackName").prev().addClass("error");
-            } else {
-              if ($("#feedbackName").prev().hasClass("error")) {
-                $("#feedbackName").prev().removeClass("error");
-              }
+            // Method in here incase they immediately try to submit without touching the form to trigger a blur event.
+            if (name == "") {
+                feedbackNameObject.prev().addClass("error");
             }
 
-            if ($("#feedbackMessage").val() == "") {
-                $("#feedbackMessage").prev().addClass("error");
-            } else {
-              if ($("#feedbackMessage").prev().hasClass("error")) {
-                $("#feedbackMessage").prev().removeClass("error");
-              }
+            if (message == "") {
+                feedbackMessageObject.prev().addClass("error");
             }
         }
 
         // Prevent the default action of the form. (As none is explicitly set in the HTML, this would be a refresh)
         event.preventDefault();
+    });
+
+    // Check to see which inputs are filled and highlight the label if not filled.
+    // We also check if they've corrected an input but not another and remove the error highlighting.
+    $(feedbackNameObject).add(feedbackMessageObject).blur(function() {
+        if ($(this).val() == "") {
+            $(this).prev().addClass("error");
+        } else {
+            if ($(this).prev().hasClass("error")) {
+                $(this).prev().removeClass("error");
+            }
+        }
     });
 });
 
